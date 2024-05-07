f6eca96a144ab022afdb0d3756afbec9f25f268d
Mds <sabawicana@gmail.com>
2024-05-07 21:23:20 +0700

    update-dhsboard

--- cargo-app/app/page.tsx
+++ cargo-app/app/page.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image'
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-200 shrink-0 items-end rounded-lg bg-gray-800 p-4 md:h-32">
         {<AcmeLogo />}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
